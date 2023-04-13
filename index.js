9bbac32784e62bd8f7b79910a864e16b5a85b33c
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-04-13 14:08:53 -0400

    first_class_function_lab_complete

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(){
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+drivers.pop('Antonia', 'Nuru')
+return ['Antonia', 'Nuru']
+
+}
+
+const returnLastTwoDrivers = function(){
+    let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    drivers.pop ('Amari', 'Mo')
+    return ['Amari', 'Mo']
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (a) => { //fare multiplier function
+    return(fare) => a*a
+}
+
+function fareDoubler(createFareMultiplier){
+return createFareMultiplier*2
+}
+
+function fareTripler(createFareMultiplier){
+    return createFareMultiplier*3
+}
+
+function selectDifferentDrivers(drivers,selectingDrivers){
+    return selectingDrivers(drivers)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+     
+
+
+
+
+    
+    
+
+
+
+
+    
+
+    
+
